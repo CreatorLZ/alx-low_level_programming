5afe95651a6498f79379a2e609df8d750ff8780c
CreatorLZ <isaacanyim3@gmail.com>
2023-06-18 09:08:52 -0700

    new script

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,17 @@
 #include <unistd.h>
+#include <stdlib.h>
 
-int main() {
-    const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", sizeof(message) - 1);
-    return 1;
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints a specific message to the standard error
+ * without using printf or puts functions.
+ *
+ * Return: Always 1 (error)
+ */
+int main(void)
+{
+    write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+    return (1);
 }
+
